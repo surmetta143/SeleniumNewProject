fee2db398552b014c16d36ed347b0bb8462146d6
nagur1babu11 <nagur1babu11@gmail.com>
2025-10-20 16:57:59 +0530

    test cases

---
+++ TestCases/test_sagar.py
@@ -0,0 +1,48 @@
+# tests/test_google_search.py
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from pages.google_search_page import GoogleSearchPage
+from TestData.data_reader import DataReader
+
+# Set the path to your WebDriver executable
+
+
+
+def test_google_search(driver):
+    search_page = GoogleSearchPage(driver)
+    search_page.load()
+    driver.maximize_window()
+    time.sleep(5)
+    search_page.setEmail("suresh@gmail.com")
+    time.sleep(5)
+    search_page.setPhone("8985256492")
+    time.sleep(5)
+
+
+def test_lekhana(driver):
+    search_page = GoogleSearchPage(driver)
+    search_page.load()
+    driver.maximize_window()
+    time.sleep(5)
+    search_page.setEmail("suresh@gmail.com")
+    time.sleep(5)
+    search_page.setPhone("8985256492")
+    time.sleep(5)
+
+
+'''def test_button_homepage(driver):
+    search_page = GoogleSearchPage(driver)
+    search_page.load()
+    assert search_page.VerifyHomeButton() == True , "not displayed"
+    assert search_page.VerifyContactButton() == True , "not displayed"
+    assert search_page.VerifyAboutButton() == True , "not displayed"
+    assert search_page.VerifyCarrerButton() == True , "not displayed"
+
+
+
+
+    time.sleep(10)'''
+
